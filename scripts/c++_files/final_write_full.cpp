720d290735676d97a2d570c6f3b713f77a7e0870
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-05-09 22:16:11 +0530

    commit final_write_full.cpp

---
+++ scripts/c++_files/final_write_full.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <limits>
+#include <istream>
+#include <string>
+int main()
+{
+	using namespace std;
+	
+	string str2;
+	string str3;
+	
+	for (int j = 0; j < 8; j++)
+	{
+		str3 = "power_full_adder_" + to_string(j) + ".txt"; 
+		str2 = "dump_full_layout" + to_string(j) + ".txt";
+		ifstream file1(str2);
+		string s2;
+		string s1 = "power";
+		string content = "";
+		string final = "";
+		while(getline(file1, s2))
+		{
+			content = s2 + "\n";
+			if(content.size() >= 3)
+				if((content.compare(4, 5, s1) == 0) && (content.compare(0, 1, "b") != 0))
+					final += content;
+				else if((content.compare(5, 5, s1) == 0) && (content.compare(0, 1, "b") != 0))
+					final += content;	
+		}
+
+		file1.close();
+		ofstream file2(str3);
+		file2 << final;
+		file2.close();
+	}
+}
